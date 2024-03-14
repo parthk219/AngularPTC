a244a17eb3e8e1f2d32123182d5e587d05156b24
parthk219 <129992000+parthk219@users.noreply.github.com>
2024-03-14 22:37:59 +0530

    Simple string interpolation
    Simple string interpolation

--- angular-ekart/src/app/app.component.ts
+++ angular-ekart/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-ekart';
+  name = 'angular-ekart';  // DATA BINDING STRING INTERPOLATION
 }
