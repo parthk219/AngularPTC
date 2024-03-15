0a264079cd2f29631b90bf912a4814bbd25bacae
parthk219 <129992000+parthk219@users.noreply.github.com>
2024-03-15 17:56:12 +0530

    css styling inline & other file
    css styling inline & other file

--- angular-ekart/src/app/header/header.component.ts
+++ angular-ekart/src/app/header/header.component.ts
@@ -2,8 +2,9 @@ import { Component } from "@angular/core";
 @Component({
     selector:'app-header',
     //template:'<h3>eKart</h3>'
-    templateUrl:'./header.component.html'
-
+    templateUrl:'./header.component.html',
+    //styles: ['a { text-decoration: none; margin: 0px 10px; }','button {padding:10px 20px}','.ekart--header{width:100%; height:100px}']
+     styleUrls:['./header.component.css']
 })
 export class HeaderComponent{
 
